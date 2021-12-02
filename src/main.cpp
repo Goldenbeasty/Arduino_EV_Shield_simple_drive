6e997ac3143774ae30bd4798dc728ddc26edb75f
Goldenbeasty <73758693+Goldenbeasty@users.noreply.github.com>
2021-12-02 17:26:25 +0200

    better gyro handling

--- src/main.cpp
+++ src/main.cpp
@@ -17,14 +17,16 @@ volatile float targetposy = 0;
 
 volatile float dist_from_target = 0;
 volatile float targetangle = 0;
+volatile double timefromlastping = 0;
+#define samplerate 100
+volatile int lastgyroangle = 0;
 
 #define motorspeed 50 // 0 - 100
 #define turnspeed 25 // Motor speed when turning
 #define wheeldiameter 35 // Wheel diameter in millimeters
-#define turnstops_per_second 25 //how many times code samples for angle per second
 
 void drive_straight_for(float dist){ // for calculating amount of distance
-  dist = (dist * 360) / (wheeldiameter * 3.1415); // find amount of degrees the wheel has to turn to reach dist (original) millimeters
+  dist = (dist * 360) / (wheeldiameter * PI); // find amount of degrees the wheel has to turn to reach dist (original) millimeters
   evshield.bank_a.motorRunDegrees(SH_Motor_Both, SH_Direction_Reverse, motorspeed, dist, SH_Completion_Wait_For, SH_Next_Action_Brake);
 }
 
@@ -34,26 +36,36 @@ float findangle(){ // you can clean it up if you wish
   return angle;
 }
 
+int gyroangle(){
+  int angle;
+  if ((millis() - timefromlastping) > samplerate){
+  angle = myGyro.getAngle();
+  lastgyroangle = angle;
+  }
+  else{
+    angle = lastgyroangle;
+  }
+  Serial.println("Current angle: " + String(angle));
+  return angle;
+}
+
 void drive_to(float targetx, float targety){ // grid is in millimeters
   Serial.println("drive to" + String(targetx) + " " + String(targety));
   targetposx = targetx;
   targetposy = targety;
   targetangle = findangle();
-  Serial.println("Current ang: " + String(myGyro.getAngle()) + " Target: " + String(targetangle));
-  while (((targetangle - 15) < myGyro.getAngle() and myGyro.getAngle() < (targetangle + 15)) != true){ // whenever targetangle is not reached, probably some play room can be given
-    Serial.print("notangle" + String(myGyro.getAngle()));
-    // Serial.print(myGyro.getAngle());
-    if (myGyro.getAngle() < targetangle){
+  while (((targetangle - 15) < gyroangle() and gyroangle() < (targetangle + 15)) != true){ // whenever targetangle is not reached, probably some play room can be given
+    Serial.print("notangle" + String(gyroangle()));
+    if (gyroangle() < targetangle){
       evshield.bank_a.motorRunUnlimited(SH_Motor_1, SH_Direction_Reverse, turnspeed);
       evshield.bank_a.motorRunUnlimited(SH_Motor_2, SH_Direction_Forward, turnspeed);
       Serial.println("right");
     }
-    else if (myGyro.getAngle() > targetangle){
+    else if (gyroangle() > targetangle){
       evshield.bank_a.motorRunUnlimited(SH_Motor_1, SH_Direction_Forward, turnspeed);
       evshield.bank_a.motorRunUnlimited(SH_Motor_2, SH_Direction_Reverse, turnspeed);
       Serial.print("left");
     }
-    // delay(1000 / turnstops_per_second); // defines how many times code samples for angle
   }
   evshield.bank_a.motorStop(SH_Motor_Both, SH_Next_Action_Brake); // stop turning once angle has been reaced
   delay(50);
@@ -68,12 +80,15 @@ void setup() {
   evshield.init(SH_HardwareI2C);
   evshield.bank_a.motorReset();
   evshield.bank_b.motorReset();
+  delay(10000);
   myGyro.init( &evshield, SH_BAS1 ); // We tell where th Gyro is connected to
+  delay(10000);
   myGyro.setMode(MODE_Gyro_Angle); // set angle mode
   delay(2000); // small delay (optional)
 }
 
 void loop() {
+  gyroangle();
   Serial.print("loop start");
   drive_to(200, 600); // this and following 4 lines of code make the robot draw a star
   drive_to(400, 0);
